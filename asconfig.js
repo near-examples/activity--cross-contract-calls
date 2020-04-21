878b4764141180000aef456b942c49f71ac892cd
Sherif Abushadi <sherif@amgando.org>
2020-04-21 17:53:33 +0300

    clarify asconfig.js contents

--- asconfig.js
+++ asconfig.js
@@ -2,11 +2,57 @@ const compile = require("near-sdk-as/compiler").compile
 
 compile("assembly/main.ts", // input file
         "out/main.wasm",    // output file
-        [
-        //   "-O1",          // Optional arguments
-        "--debug",
-        "--measure",         // Shows compiler runtime
-        "--validate"         // Validate the generated wasm module
+        [                   // add optional args here
+          "--debug",        // Shows debug output
+          "--measure",      // Shows compiler run time
+          "--validate"      // Validate the generated wasm module
         ], {
-          verbose: true     // Output the cli args passed to asc
+          verbose: false    // Output the cli args passed to asc
         });
+
+/**************************************************************
+  NEAR relies on AssemblyScript to optimize file size and 
+  execution speed which makes for more cost effective contracts
+***************************************************************
+
+To make the contract in this project as small and fast as 
+possible just add the following optional argument to the 
+compile() methods 3rd argument, the array of optional args
+
+-O3z --converge
+
+So the above call to compile() would change to this: 
+
+compile("assembly/main.ts", // input file
+        "out/main.wasm",    // output file
+        [                   // add optional args here
+          "-O3z --converge" // Optimize for size and speed
+          "--debug",        // Shows debug output
+          "--measure",      // Shows compiler run time
+          "--validate"      // Validate the generated wasm module
+        ], {
+          verbose: false    // Output the cli args passed to asc
+        });
+
+***************************************************************
+
+All optional arguments are documented on this page 
+https://docs.assemblyscript.org/details/compiler
+
+with the relevant example about optimizing for size and speed 
+included below as well
+
+--optimize, -O    Optimizes the module.Typical shorthands are:
+
+                  Default optimizations   -O / -O3s
+                  Make a release build    -O --noAssert
+                  Make a debug build      --debug
+                  Optimize for speed      -O3
+                  Optimize for size       -O3z --converge
+
+--optimizeLevel   How much to focus on optimizing code.[0 - 3]
+--shrinkLevel     How much to focus on shrinking code size.[0 - 2, s = 1, z = 2]
+--converge        Re-optimizes until no further improvements can be made.
+--noAssert        Replaces assertions with just their value without trapping
+
+*/
