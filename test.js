3111c9d576374816ac7d1f6120613b1f0133cc0e
Sherif Abushadi <sherif@amgando.org>
2020-04-29 13:07:36 +0300

    clean slate

--- test.js
+++ test.js
@@ -6,15 +6,15 @@ let runtime = new Runtime();
 let alice = runtime.newAccount("alice");
 runtime.newAccount("rust", "rust/res/rust_example.wasm");
 
-let res  = (alice.call_other("rust", "reverse", {word: {text: "hello world"}}))
-console.log(JSON.stringify(res, null , 2));
+let res = (alice.call_other("rust", "reverse", { word: { text: "hello world" } }))
+console.log(JSON.stringify(res, null, 2));
 // runtime.newAccount("words.examples", "out/words.wasm")
 
-// assert(alice.call_other("sentences", "reverseWordOne").return_data.text == "elpmas",
+assert(alice.call_other("sentences", "reverseWordOne").return_data.text == "elpmas")
+
 //        "text should be reversed");
 
 // assert(alice.call_other("sentences", "reverseWordTwo"))
 
 // TODO: this still breaks, so for now no passing to call back method.
 // alice.call_other("sentences", "reverseWordThree");
-
